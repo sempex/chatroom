eac8fe8e4cec295e011ff81a67ce8730ccd9706e
Tim Reber <tim.reber@bluewin.ch>
2022-06-16 18:46:16 +0200

    added Front page (Enter room id field and submit button)

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
-    <h1 className="text-red-500 text-center font-extrabold">Hallo Noe, wie geht es dir heute?</h1>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <form>
+        <div className="">
+          <input type="text" placeholder="Enter Room ID" className="p-6 bg-slate-200 rounded-xl text-center"></input>
+        </div>
+        <div className="text-center mt-2">
+          <input type="submit" value="Continue" className="py-4 px-7 bg-teal-300 rounded-xl font-extrabold"></input>
+        </div>
+      </form>
     </div>
   )
 }
