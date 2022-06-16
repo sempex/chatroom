224dc00b9db64e6104cf860f4b2bbd51d39ee7ea
Tim Reber <tim.reber@bluewin.ch>
2022-06-16 18:56:19 +0200

    added shadows

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,10 @@ export default function Home() {
     <div className="flex flex-col justify-center items-center h-screen">
       <form>
         <div className="">
-          <input type="text" placeholder="Enter Room ID" className="p-6 bg-slate-200 rounded-xl text-center"></input>
+          <input type="text" placeholder="Enter Room ID" className="p-6 bg-slate-200 rounded-xl text-center shadow-md shadow-slate-500/50"></input>
         </div>
         <div className="text-center mt-2">
-          <input type="submit" value="Continue" className="py-4 px-7 bg-teal-300 rounded-xl font-extrabold"></input>
+          <input type="submit" value="Continue" className="py-4 px-7 bg-teal-300 rounded-xl font-extrabold shadow-md shadow-teal-300/60"></input>
         </div>
       </form>
     </div>
