9e6d84c35f621bcb220de305d20b8e00281f6e78
Tim Reber <tim.reber@bluewin.ch>
2022-06-16 23:30:05 +0200

    added welcome sentence

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="font-bold text-4xl mb-28">Welcome to <span className="font-extrabold text-teal-300">SimpleChat!</span></h1>
       <form>
         <div className="">
           <input type="text" placeholder="Enter Room ID" className="p-6 bg-slate-200 rounded-xl text-center shadow-md shadow-slate-500/50"></input>
