8f3a9da5aeac2f51f6457dd15a3bc70b89ef8b91
Tim Reber <tim.reber@bluewin.ch>
2022-06-17 18:00:54 +0200

    designed typewritter

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,14 @@ import { useTypewriter } from "react-simple-typewriter"
 
 export default function Home() {
   const { text, count } = useTypewriter({
-    words: ['Connect', 'Chat', 'Call']
+    words: ['Connect with new people!', 'Meet friends!', 'Just chat!', 'Get work done!'],
+    loop: false
   })
   return (
     <div className="flex flex-col justify-center items-center h-screen relative">
       <img src="/assets/background.svg" alt="Background Image" className="absolute inset-0 h-screen w-screen z-[-1]"/>
-      <h1 className="font-bold text-4xl mb-28">Welcome to <span className="font-extrabold text-teal-300">SimpleChat!</span></h1>
-      <p>With Simple Chat you can {text}</p>
+      <h1 className="font-bold text-4xl mb-5">Welcome to <span className="font-extrabold text-teal-300">SimpleChat!</span></h1>
+      <p className="mb-28 font-bold text-xl">With SimpleChat you can <span className="font-extrabold text-gray-600 text-2xl">{text}</span></p>
       <form>
         <div className="">
           <input type="text" placeholder="Enter Room ID" className="p-6 bg-slate-200 rounded-xl text-center shadow-md shadow-slate-500/50"></input>
