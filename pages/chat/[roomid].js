9457aa036dd9e4b3bc8f69387af079febc7f429e
Tim Reber <tim.reber@bluewin.ch>
2022-06-20 00:08:23 +0200

    Working without initial id getting

--- pages/chat/[roomid].js
+++ pages/chat/[roomid].js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import io from 'socket.io-client'
 let socket
 
@@ -9,7 +9,6 @@ export default function Home() {
     const router = useRouter()
     const id = router.query.roomid
     useEffect(() => {
-       async () => {
         const socketInitializer = async () => {
             await fetch('/api/socket')
             socket = io()
@@ -18,7 +17,8 @@ export default function Home() {
             })
         }
         if (!socket) socketInitializer()
-        socket.emit('join', id) }
+        else socket.emit('join', id)
+        return () => { }
     }, [router])
 
 
