b6ddb7f4137733d0c12e09b600efdf4f866db72e
Tim Reber <tim.reber@bluewin.ch>
2022-06-23 16:11:52 +0200

    smooth scrolling

--- pages/chat/[roomid].js
+++ pages/chat/[roomid].js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import io from 'socket.io-client'
 import Message from "../../components/Message"
 let socket
@@ -9,6 +9,7 @@ export default function Home() {
 
     const [message, setMessage] = useState('')
     const [incomingMessages, setIncomingMessages] = useState([])
+    const refLatestChat = useRef(null)
     const router = useRouter()
     const id = router.query.roomid
     useEffect(() => {
@@ -21,6 +22,9 @@ export default function Home() {
             })
             socket.on('serverMessage', message => {
                 setIncomingMessages(current => [...current, message])
+                refLatestChat.current.scrollIntoView({
+                    behavior: "smooth"
+                })
             })
             if (!ignore) socket.emit('join', id)
         }
@@ -43,14 +47,16 @@ export default function Home() {
     return (
         <div className="relative h-screen flex flex-col">
             <img src="/assets/background.svg" alt="Background Image" className="absolute inset-0 h-full w-full z-[-1]" />
-            <div className="overflow-y-auto">
+            <div className="overflow-y-auto h-full">
                 {
                     incomingMessages.map((message, i) => {
                         return <Message messagetext={message} key={i} />
                     })
                 }
+                <div ref={refLatestChat} className="mb-20"></div>
+
             </div>
-            <form className="flex justify-center items-end h-full" onSubmit={sendMessage}>
+            <form className="flex justify-center" onSubmit={sendMessage}>
                 <div className="space-x-6 mb-10">
                     <input className="px-7 py-2 bg-slate-200 rounded-xl shadow-md shadow-slate-500/50" onChange={onChangeHandler} value={message} />
                     <input type={"submit"} value="Send" className="py-2 px-7 bg-teal-300 rounded-xl font-extrabold shadow-md shadow-teal-300/60" />
