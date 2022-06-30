ff68aa90ae670e9320a27bfb51bb8be850ce9bfb
Tim Reber <tim.reber@bluewin.ch>
2022-06-30 14:53:05 +0200

    slkdfg

--- components/Message.js
+++ components/Message.js
@@ -1,11 +1,13 @@
 import { useSession } from "next-auth/react"
-export default function Message({ messagetext, messageUsername }) {
+export default function Message({ messagetext, messageUsername, messageProfilepicture }) {
     const { data: session, status } = useSession()
     
     const selfMessage = session?.user.name === messageUsername
     
     return (
         <div className={`${selfMessage ? 'ml-auto' : ""} rounded-b-xl rounded-tr-xl bg-slate-100 w-fit max-w-[25%] m-4 px-5 py-2`}>
+            <p className="font-bold text-xs">{messageUsername}</p>
+            {/* <img src={messageProfilepicture} /> */}
             <p className="font-medium text-sm break-all">{messagetext}</p>
         </div>
     )
