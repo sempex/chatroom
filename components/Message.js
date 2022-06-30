fcaf66eaf6ae48df6c691b96b1504574ff2ada93
Tim Reber <tim.reber@bluewin.ch>
2022-06-30 16:41:35 +0200

    corner personalisation

--- components/Message.js
+++ components/Message.js
@@ -5,7 +5,7 @@ export default function Message({ messagetext, messageUsername, messageProfilepi
     const selfMessage = session?.user.name === messageUsername
     
     return (
-        <div className={`${selfMessage ? 'ml-auto' : ""} rounded-b-xl rounded-tr-xl bg-slate-100 w-fit max-w-[25%] m-4 px-5 py-2`}>
+        <div className={`${selfMessage ? 'ml-auto rounded-tl-xl' : "rounded-tr-xl"} rounded-b-xl bg-slate-100 w-fit max-w-[25%] m-4 px-5 py-2`}>
             <p className="font-bold text-xs">{messageUsername}</p>
             {/* <img src={messageProfilepicture} /> */}
             <p className="font-medium text-sm break-all">{messagetext}</p>
