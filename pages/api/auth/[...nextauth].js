b02ddac15a5c560572934f99dcc02c355f8b16e2
Tim Reber <tim.reber@bluewin.ch>
2022-07-01 17:57:04 +0200

    not working sign in with creds

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 
@@ -8,10 +9,31 @@ const prisma = new PrismaClient()
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      id: "creds",
+      async authorize(credentials, req) {
+        const user = prisma.user.findMany({
+          where: {
+            AND: [
+              { name: credentials.username },
+              { password: credentials.password }
+            ]
+          }
+        })
+        if (user) {
+          console.log(user)
+          return user
+        }
+        else consle.log("no User")
+      }
+
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+
   ],
   secret: process.env.SECRET,
 })
