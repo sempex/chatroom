815c17ea893194a23846e5f336b09b657b95074e
Tim Reber <tim.reber@bluewin.ch>
2022-07-01 14:15:29 +0200

    migrated login

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,14 @@
 import { useEffect, useState } from "react"
 import { useSession } from "next-auth/react"
-import Login from "../components/Login"
 import Landing from "../components/Landing"
+import { Router, useRouter } from "next/router"
 export default function Home() {
   const { data: session, status } = useSession()
-  const [ login, setLogin ] = useState(true)
-  useEffect(() => {
-    const checkAuthenticated = async () => {
-      if (status === "authenticated") {
-        await setLogin(false)
-      }
-    }
-    checkAuthenticated()
-  })
-  return (
+  const [login, setLogin] = useState(true)
+  const router = useRouter()
 
-    <div>
-      {
-        login ? <Login /> : <Landing />
-      }
-    </div>
-  )
+  if (status !== "loading" && status !== "authenticated") router.push('/login')
+    return (
+      <Landing />
+    )
 }
